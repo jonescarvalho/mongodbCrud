ee87ea6325a8efe7c6bd1fc23f8e23c6f9b6c730
Jones Carvalho <jonescarva@gmail.com>
2018-02-16 09:39:35 -0200

    Add files via upload

---
+++ src/myApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace myApp
+{
+    class Program
+    {
+        private static string conString = "mongodb://localhost:27017";
+        private static string database = "teste";
+        private static string collectionName = "lst";
+        static void Main(string[] args)
+        {
+            getItems(args).Wait();
+            Console.ReadLine();
+        }
+
+        static async Task getItems(string[] args)
+        {
+            //Console.WriteLine("---------- Insert ----------");
+
+            //var document = new BsonDocument
+            //{
+            //    {"_id",5} 
+            //};
+
+            //document.Add("adicionado", 17);
+            //document["Level"] = "AHOTAVIO"; 
+
+            //var arr = new BsonArray();
+            //arr.Add(new BsonDocument("Jones", "Carvalho"));
+
+            //document.Add("Hired", arr);
+
+            //var collection = returnDatabaseCollection();
+
+            //collection.InsertOneAsync(document);
+
+
+            Console.WriteLine("---------- Response Methods MongoDB ----------");
+            var collection = returnDatabaseCollection();
+
+            Console.WriteLine("-- Method 1 --");
+            using (var cursor = await collection.Find(new BsonDocument()).ToCursorAsync())
+                while (await cursor.MoveNextAsync())
+                    foreach (var doc in cursor.Current)
+                        Console.WriteLine(doc);
+
+            Console.WriteLine("-- Method 2 --");
+            var list = await collection.Find(new BsonDocument()).ToListAsync();
+            foreach (var dox in list)
+                Console.WriteLine(dox);
+
+            Console.WriteLine("--    Method 3 --");
+            await collection.Find(new BsonDocument()).ForEachAsync(X => Console.WriteLine(X));
+        }
+
+        private static IMongoCollection<BsonDocument> returnDatabaseCollection()
+        {
+            var Client = new MongoClient(conString);
+            var DB = Client.GetDatabase(database);
+            var collection = DB.GetCollection<BsonDocument>(collectionName);
+            return collection;
+        }
+    }
+}
+
---
+++ src/myApp/myClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myApp
+{
+    public class myClass
+    {
+        public int _id { get; set; }
+        public string Name { get; set; }
+    }
+}
